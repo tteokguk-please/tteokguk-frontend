ca8487d4c317387d94c4f1260d1c3e0b21ce3ea4
Kim-Jaemin420 <jany240@naver.com>
2024-01-18 16:08:37 +0900

    Chore: 컬러 변수 설정

--- panda.config.ts
+++ panda.config.ts
@@ -12,6 +12,26 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
+    tokens: {
+      colors: {
+        white: { value: "#FFFFFF" },
+        back: { value: "#FBF8EF" },
+        black: { value: "#1B1B1B" },
+        gray: { 50: { value: "#929292" } },
+        primary: {
+          10: { value: "#F5EFDF" },
+          45: { value: "#E8E3D2" },
+          60: { value: "#EEE2BE" },
+          100: { value: "#F8D97E" },
+        },
+        secondary: {
+          50: { value: "#B3D6D4" },
+          100: { value: "#83C7C3" },
+        },
+        red: { 100: { value: "#F63333" } },
+        green: { 100: { value: "#37C35E" } },
+      },
+    },
     extend: {},
   },
 
