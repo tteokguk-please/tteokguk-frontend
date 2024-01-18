bf10a59c3eccfef9ed4619846bedde817c5d7d9f
Kim-Jaemin420 <jany240@naver.com>
2024-01-18 13:30:02 +0900

    Feat: 페이지 상수들 추가

---
+++ src/constants/route.ts
@@ -0,0 +1,10 @@
+export const HOME_PAGE = "/";
+export const LOGIN_PAGE = "/login";
+export const EMAIL_LOGIN_PAGE = "/login/email";
+export const SIGNUP_PAGE = "/signup";
+export const MY_PAGE = "/my-page";
+export const MY_ACTIVITY_PAGE = "/my-page/activity";
+export const TTEOKGUK_GALLERY_PAGE = "/tteokguks";
+export const TTEOKGUK_COOKING_PAGE = "/tteokguk-cooking";
+export const INGREDIENT_ADDITION_PAGE = "/add-ingredients";
+export const RANDOM_TTEOKGUK_PAGE = "/random-tteokguk";
