e5c4f2fa94f208622dda998d02750f8efb6361c1
Kim-Jaemin420 <jany240@naver.com>
2024-01-19 15:38:55 +0900

    Feat: overlay provider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { OverlayProvider } from "@toss/use-overlay";
 
-import App from './App.tsx';
-import './index.css';
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <OverlayProvider>
+      <App />
+    </OverlayProvider>
+  </React.StrictMode>,
 );
