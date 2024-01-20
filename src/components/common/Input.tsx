02c1b8f4245a2a443a06bed12707bfe9861200cc
Kim-Jaemin420 <jany240@naver.com>
2024-01-21 02:20:55 +0900

    Feat: 스타일이 반복되는 input, label을 common으로 분리

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,33 @@
+import { InputHTMLAttributes } from "react";
+
+import classnames from "classnames";
+
+import { css } from "@styled-system/css";
+
+const Input = ({ id, type, placeholder, className }: InputHTMLAttributes<HTMLInputElement>) => {
+  return (
+    <input
+      id={id}
+      type={type}
+      placeholder={placeholder}
+      className={classnames(styles.input, className)}
+    />
+  );
+};
+
+export default Input;
+
+const styles = {
+  input: css({
+    width: "100%",
+    height: "3.7rem",
+    backgroundColor: "primary.20",
+    borderRadius: "1rem",
+    paddingX: "1.6rem",
+    outline: "none",
+    _placeholder: {
+      color: "black",
+      fontSize: "1.2rem",
+    },
+  }),
+};
