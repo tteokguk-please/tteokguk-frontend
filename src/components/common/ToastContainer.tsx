bd988aa41ff5be2ab940eefb7bf0152b095a62e9
Kim-Jaemin420 <jany240@naver.com>
2024-01-21 02:01:07 +0900

    Feat: sonner Toast 커스텀

---
+++ src/components/common/ToastContainer.tsx
@@ -0,0 +1,26 @@
+import { Toaster } from "sonner";
+
+const ToastContainer = () => {
+  return (
+    <Toaster
+      position="bottom-center"
+      visibleToasts={1}
+      toastOptions={{ duration: 20000, style: styles.container }}
+    />
+  );
+};
+
+export default ToastContainer;
+
+const styles = {
+  container: {
+    diplay: "flex",
+    justifyContent: "center",
+    fontSize: "1.5rem",
+    height: "4.2rem",
+    backgroundColor: "#929292",
+    color: "#ffffff",
+    marginBottom: "1.2rem",
+    borderRadius: "0.8rem",
+  },
+};
