2a21cd4ab3bf1570fd7993c1ead3e27328d111b9
Kim-Jaemin420 <jany240@naver.com>
2024-01-20 20:59:56 +0900

    Modify: 디자인 시스템에 맞게 props 받도록 구현

--- src/components/common/BaseButton.tsx
+++ src/components/common/BaseButton.tsx
@@ -4,9 +4,17 @@ import classnames from "classnames";
 
 import { css } from "@styled-system/css";
 
-const BaseButton = ({ children, onClick, className }: ButtonHTMLAttributes<HTMLButtonElement>) => {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color: "primary.100" | "primary.45" | "secondary" | "white" | "yellow";
+  applyColorTo: "background" | "outline";
+  size: "large" | "medium";
+}
+
+const BaseButton = ({ children, onClick, className, color, applyColorTo, size }: Props) => {
+  const buttonStyle = classnames(styles.button, styles[applyColorTo][color], styles[size]);
+
   return (
-    <button className={classnames(styles.button, className)} onClick={onClick}>
+    <button className={classnames(buttonStyle, className)} onClick={onClick}>
       {children}
     </button>
   );
@@ -16,11 +24,32 @@ export default BaseButton;
 
 const styles = {
   button: css({
-    width: "28rem",
-    height: "5.1rem",
     fontSize: "1.6rem",
     fontWeight: 700,
     borderRadius: "1.2rem",
+    backgroundColor: "white",
     cursor: "pointer",
+  }),
+  background: {
+    "primary.100": css({ backgroundColor: "primary.100" }),
+    "primary.45": css({ borderColor: "primary.45" }),
+    secondary: css({ backgroundColor: "secondary.100" }),
+    white: css({ backgroundColor: "white" }),
+    yellow: css({ backgroundColor: "yellow.100" }),
+  },
+  outline: {
+    "primary.100": css({ borderColor: "primary.100" }),
+    "primary.45": css({ borderColor: "primary.45" }),
+    secondary: css({ borderColor: "secondary.100" }),
+    white: css({ borderColor: "white" }),
+    yellow: css({ borderColor: "yellow.100" }),
+  },
+  large: css({
+    width: "31.2rem",
+    height: "5.1rem",
+  }),
+  medium: css({
+    width: "28rem",
+    height: "5.1rem",
   }),
 };
