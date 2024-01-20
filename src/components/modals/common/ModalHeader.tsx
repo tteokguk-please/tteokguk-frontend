fe2c2b2ea8baa2cfff4bb0730c7b2e79bab725c2
Kim-Jaemin420 <jany240@naver.com>
2024-01-20 20:59:03 +0900

    Feat: modal을 컴파운드 컴포넌트로 분리

---
+++ src/components/modals/common/ModalHeader.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from "react";
+
+import { css } from "@styled-system/css";
+
+import CloseIcon from "@/assets/svg/close.svg";
+
+interface Props {
+  children: ReactNode;
+  handleClickClose: () => void;
+}
+
+const ModalHeader = ({ children, handleClickClose }: Props) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>{children}</div>
+      <button onClick={handleClickClose} className={styles.closeButton}>
+        <CloseIcon />
+      </button>
+    </div>
+  );
+};
+
+export default ModalHeader;
+
+const styles = {
+  container: css({
+    display: "flex",
+    position: "relative",
+  }),
+
+  title: css({
+    flex: 1,
+    textAlign: "center",
+    marginTop: "1.2rem",
+  }),
+  closeButton: css({
+    position: "absolute",
+    right: 0,
+    top: 0,
+    cursor: "pointer",
+  }),
+};
