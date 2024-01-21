5ddab2ec0c9d508d7f8793e3235aaac4c2d0819d
Kim-Jaemin420 <jany240@naver.com>
2024-01-22 05:24:13 +0900

    Feat: page에 Layout 적용

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -12,6 +12,8 @@ import MyActivityPage from "@/pages/MyActivityPage";
 import RandomUserPage from "@/pages/RandomUserPage";
 import NotFoundPage from "@/pages/NotFoundPage";
 
+import Layout from "@/components/layout/Layout";
+
 export type RoutePath =
   | "/"
   | "/login"
@@ -29,17 +31,18 @@ export type RoutePath =
 export const Routes = () => {
   return (
     <ReactRoutes>
-      <Route path="/" />
-      <Route index element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="login/email" element={<EmailLoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/tteokguks" element={<MainPage />} />
-      <Route path="/tteokguks/:id" element={<TteokgukPage />} />
-      <Route path="/tteokguk/create" element={<TteokgukCookingPage />} />
-      <Route path="/my-page" element={<MyPage />} />
-      <Route path="/my-page/activity" element={<MyActivityPage />} />
-      <Route path="/users/:id" element={<RandomUserPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="login/email" element={<EmailLoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/tteokguks" element={<MainPage />} />
+        <Route path="/tteokguks/:id" element={<TteokgukPage />} />
+        <Route path="/tteokguk/create" element={<TteokgukCookingPage />} />
+        <Route path="/my-page" element={<MyPage />} />
+        <Route path="/my-page/activity" element={<MyActivityPage />} />
+        <Route path="/users/:id" element={<RandomUserPage />} />
+      </Route>
       <Route path="/*" element={<NotFoundPage />} />
     </ReactRoutes>
   );
