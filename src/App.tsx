cd810f8601bcedf19c3cec535f15b03265d4209f
Kim-Jaemin420 <jany240@naver.com>
2024-01-22 19:04:37 +0900

    Feat: App에 ToastContainer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import { OverlayProvider } from "@toss/use-overlay";
 
 import { Routes } from "./routes/Routes";
+import ToastContainer from "./components/common/ToastContainer";
 
 const App = () => {
   return (
     <BrowserRouter>
       <OverlayProvider>
+        <ToastContainer />
         <Routes />
       </OverlayProvider>
     </BrowserRouter>
