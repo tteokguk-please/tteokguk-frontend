9af9093e1ebda6bdb05f94fa77c1437cf9aad7a1
Kim-Jaemin420 <jany240@naver.com>
2024-01-24 23:56:27 +0900

    Feat: 떡국 리스트와 카드 컴포넌트를 사용해서 페이지 UI 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,81 @@
+import { useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+
+import { css } from "@styled-system/css";
+
+import TteokgukList from "@/components/common/Tteokguks/TteokgukList";
+import Button from "@/components/common/Button";
+
 const MainPage = () => {
-  return <div>떡국 리스트를 볼 수 있는 페이지입니다.</div>;
+  const [tabIndex, setTabIndex] = useState(0);
+
+  return (
+    <div className={styles.container}>
+      <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList className={styles.tabList}>
+          <Tab className={tabIndex === 0 ? styles.selectedTab : ""}>새로운 떡국</Tab>
+          <Tab className={tabIndex === 1 ? styles.selectedTab : ""}>완성된 떡국</Tab>
+        </TabList>
+        <TabPanel className={styles.tabPanel}>
+          <TteokgukList />
+        </TabPanel>
+        <TabPanel className={styles.tabPanel}>
+          <TteokgukList />
+        </TabPanel>
+      </Tabs>
+
+      <Button color="secondary" applyColorTo="background" className={styles.button}>
+        소원 떡국 만들기
+      </Button>
+    </div>
+  );
 };
 
 export default MainPage;
+
+const styles = {
+  container: css({
+    position: "relative",
+    height: "100%",
+    overflow: "auto",
+    _scrollbar: {
+      display: "none",
+    },
+  }),
+  tabList: css({
+    display: "flex",
+    justifyContent: "space-around",
+    fontSize: "1.6rem",
+    fontWeight: 700,
+    borderBottomWidth: "0.1rem",
+    borderBottomColor: "primary.45",
+    padding: "0.8rem 5.6rem 0.9rem",
+    cursor: "pointer",
+  }),
+  tabPanel: css({
+    display: "flex",
+    flexFlow: "column wrap",
+    marginTop: "2rem",
+    paddingLeft: "1.6rem",
+  }),
+
+  selectedTab: css({
+    position: "relative",
+    _after: {
+      content: '""',
+      position: "absolute",
+      bottom: "-0.9rem",
+      left: "50%",
+      transform: "translateX(-50%)",
+      width: "9.4rem",
+      height: "0.4rem",
+      backgroundColor: "primary.100",
+    },
+  }),
+  button: css({
+    position: "sticky",
+    width: "calc(100% - 5rem)",
+    bottom: "3rem",
+    marginX: "2.4rem",
+  }),
+};
