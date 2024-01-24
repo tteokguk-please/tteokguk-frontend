f96c472fd8567bd4aa1b3d9296db6349af8caca2
Kim-Jaemin420 <jany240@naver.com>
2024-01-25 05:59:23 +0900

    Feat: 활동 내역 페이지 UI 구현

--- src/pages/MyActivityPage.tsx
+++ src/pages/MyActivityPage.tsx
@@ -1,5 +1,67 @@
+import { Fragment, useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+
+import { css } from "@styled-system/css";
+
+import Header from "@/components/common/Header";
+import ReceivedIngredientsList from "@/components/MyActivityPage/ReceivedIngredientsList";
+
 const MyActivityPage = () => {
-  return <div>내 활동 내역 페이지 입니다.</div>;
+  const [tabIndex, setTabIndex] = useState(0);
+
+  return (
+    <Fragment>
+      <Header hasPreviousPage>활동 내역</Header>
+      <div className={styles.container}>
+        <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+          <TabList className={styles.tabList}>
+            <Tab className={tabIndex === 0 ? styles.selectedTab : ""}>받은 떡국 재료</Tab>
+            <Tab className={tabIndex === 1 ? styles.selectedTab : ""}>내가 응원한 떡국</Tab>
+          </TabList>
+          <TabPanel className={styles.tabPanel}>
+            <ReceivedIngredientsList />
+          </TabPanel>
+          <TabPanel className={styles.tabPanel}></TabPanel>
+        </Tabs>
+      </div>
+    </Fragment>
+  );
 };
 
 export default MyActivityPage;
+
+const styles = {
+  container: css({
+    height: "calc(100% - 4.8rem)",
+    paddingX: "2.4rem",
+  }),
+  tabList: css({
+    display: "flex",
+    justifyContent: "space-around",
+    fontSize: "1.6rem",
+    fontWeight: 700,
+    borderBottomWidth: "0.1rem",
+    borderBottomColor: "primary.45",
+    padding: "0.8rem 5.6rem 0.9rem",
+    marginBottom: "2rem",
+    cursor: "pointer",
+  }),
+  tabPanel: css({
+    display: "flex",
+    flexFlow: "column wrap",
+  }),
+  selectedTab: css({
+    position: "relative",
+    outline: "none",
+    _after: {
+      content: '""',
+      position: "absolute",
+      bottom: "-0.9rem",
+      left: "50%",
+      transform: "translateX(-50%)",
+      width: "9.4rem",
+      height: "0.4rem",
+      backgroundColor: "primary.100",
+    },
+  }),
+};
