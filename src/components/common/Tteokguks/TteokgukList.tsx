32a5a8a32929ea9050403e828abdad94ab1de11a
Kim-Jaemin420 <jany240@naver.com>
2024-01-24 23:56:00 +0900

    Feat: 떡국 리스트 UI 구현

---
+++ src/components/common/Tteokguks/TteokgukList.tsx
@@ -0,0 +1,23 @@
+import { css } from "@styled-system/css";
+
+import TteokgukCard from "./TteokgukCard";
+
+const TteokgukList = () => {
+  return (
+    <ul className={styles.container}>
+      {[...Array(12)].map(() => (
+        <TteokgukCard tteokgukNumber={1178} nickname="재민" />
+      ))}
+    </ul>
+  );
+};
+
+export default TteokgukList;
+
+const styles = {
+  container: css({
+    display: "flex",
+    flexFlow: "row wrap",
+    gap: "1.6rem",
+  }),
+};
