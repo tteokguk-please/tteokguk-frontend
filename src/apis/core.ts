a2a71c0bb458ce88a687087a0fd01fe0dadf6579
Kim-Jaemin420 <jany240@naver.com>
2024-01-25 23:28:09 +0900

    Feat: ky를 사용하여 api core 로직 구현

---
+++ src/apis/core.ts
@@ -0,0 +1,52 @@
+import ky, { Input, Options } from "ky";
+
+const HTTP_METHODS = {
+  GET: "get",
+  POST: "post",
+  PATCH: "patch",
+  PUT: "put",
+  DELETE: "delete",
+} as const;
+
+type HttpMethod = "get" | "post" | "put" | "patch" | "delete";
+
+const kyInstance = ky.create({
+  prefixUrl: import.meta.env.VITE_BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  hooks: {
+    beforeRequest: [
+      (request) => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+          request.headers.set("Authorization", `Bearer ${token}`);
+        }
+      },
+    ],
+    afterResponse: [
+      (request, options, response) => {
+        if (!response.ok) {
+          return Promise.reject(response);
+        }
+        return response;
+      },
+    ],
+  },
+});
+const createApiMethod =
+  (method: HttpMethod) =>
+  async <T>(url: Input, options: Options = {}): Promise<T> => {
+    const response = await kyInstance(url, { ...options, method });
+    return response.json<T>();
+  };
+
+export default {
+  get: createApiMethod(HTTP_METHODS.GET),
+  post: createApiMethod(HTTP_METHODS.POST),
+  patch: createApiMethod(HTTP_METHODS.PATCH),
+  put: createApiMethod(HTTP_METHODS.PUT),
+  delete: createApiMethod(HTTP_METHODS.DELETE),
+};
