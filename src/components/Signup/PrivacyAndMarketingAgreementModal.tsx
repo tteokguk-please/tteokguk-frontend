db0815d65778627b6b4878b703a05193f882ef1f
Kim-Jaemin420 <jany240@naver.com>
2024-01-26 03:21:37 +0900

    Modify: modal header 제거

--- src/components/Signup/PrivacyAndMarketingAgreementModal.tsx
+++ src/components/Signup/PrivacyAndMarketingAgreementModal.tsx
@@ -14,58 +14,56 @@ const PrivacyAndMarketingAgreementModal = ({ isOpen, close }: Props) => {
   return (
     isOpen && (
       <Modal>
-        <Modal.Header className={classnames(styles.header)}>
-          개인정보 수집·이용 동의서 (필수)
-        </Modal.Header>
-        <Modal.Body className={styles.content}>
-          <div className={styles.guide}>
-            ‘떡국을 부탁해’의 개인정보 수집 이용 목적은 다음과 같습니다.
+        <Modal.Body>
+          <div className={styles.content}>
+            <div className={styles.header}>개인정보 수집·이용 동의서 (필수)</div>
+            <div className={styles.guide}>
+              ‘떡국을 부탁해’의 개인정보 수집 이용 목적은 다음과 같습니다.
+            </div>
+            <div className={styles.tableContainer}>
+              <table className={styles.table}>
+                <thead className={styles.tableHeader}>
+                  <tr>
+                    <th>수집항목</th>
+                    <th>수집목적</th>
+                    <th>기간</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>이메일</td>
+                    <td>회원 서비스 및 서비스 제공</td>
+                    <td>탈퇴 시</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
-          <div className={styles.tableContainer}>
-            <table className={styles.table}>
-              <thead className={styles.tableHeader}>
-                <tr>
-                  <th>수집항목</th>
-                  <th>수집목적</th>
-                  <th>기간</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>이메일</td>
-                  <td>회원 서비스 및 서비스 제공</td>
-                  <td>탈퇴 시</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </Modal.Body>
-        <Modal.Header className={classnames(styles.header, styles.marketingHeader)}>
-          마케팅 · 홍보 활용 동의서 (선택)
-        </Modal.Header>
-        <Modal.Body className={styles.content}>
-          <div className={styles.guide}>
-            사용자가 ‘떡국을 부탁해’에서 작성한 내용은 마케팅 및 홍보에 활용 될 수 있습니다.
-          </div>
-          <div className={styles.tableContainer}>
-            <table className={styles.table}>
-              <thead className={styles.tableHeader}>
-                <tr>
-                  <th>수집항목</th>
-                  <th>수집목적</th>
-                  <th>기간</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <span className={styles.block}>사용자</span> 작성 소원
-                  </td>
-                  <td>마케팅 및 홍보 활용</td>
-                  <td>탈퇴 시</td>
-                </tr>
-              </tbody>
-            </table>
+          <div className={classnames(styles.content, styles.marketing)}>
+            <div className={styles.header}>마케팅 · 홍보 활용 동의서 (선택)</div>
+            <div className={styles.guide}>
+              사용자가 ‘떡국을 부탁해’에서 작성한 내용은 마케팅 및 홍보에 활용 될 수 있습니다.
+            </div>
+            <div className={styles.tableContainer}>
+              <table className={styles.table}>
+                <thead className={styles.tableHeader}>
+                  <tr>
+                    <th>수집항목</th>
+                    <th>수집목적</th>
+                    <th>기간</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <span className={styles.block}>사용자</span> 작성 소원
+                    </td>
+                    <td>마케팅 및 홍보 활용</td>
+                    <td>탈퇴 시</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
           <div className={styles.caution}>
             <span className={styles.block}>*사용자는 동의를 거부할 권리가 있으며, </span>
@@ -100,6 +98,9 @@ const styles = {
     textAlign: "center",
     paddingX: "1.9rem",
     fontSize: "1.4rem",
+  }),
+  marketing: css({
+    marginTop: "2.4rem",
   }),
   tableContainer: css({
     borderWidth: "0.1rem",
