2095bbe703fcae855d4720def34a82de620a28ee
Kim-Jaemin420 <jany240@naver.com>
2024-01-25 22:51:46 +0900

    Feat: 회원가입, 로그인에서 사용될 공통된 디자인의 Input, label

---
+++ src/components/common/Label.tsx
@@ -0,0 +1,24 @@
+import { LabelHTMLAttributes } from "react";
+
+import classnames from "classnames";
+
+import { css } from "@styled-system/css";
+
+const Label = ({ htmlFor, children, className }: LabelHTMLAttributes<HTMLLabelElement>) => {
+  return (
+    <label htmlFor={htmlFor} className={classnames(styles.label, className)}>
+      {children}
+    </label>
+  );
+};
+
+export default Label;
+
+const styles = {
+  label: css({
+    display: "block",
+    fontSize: "1.4rem",
+    fontWeight: 700,
+    marginBottom: "0.6rem",
+  }),
+};
