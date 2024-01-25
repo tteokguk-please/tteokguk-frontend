29a15a7d436485de908a573d3bc4c15e9189675d
J'aimemin <61978339+Kim-Jaemin420@users.noreply.github.com>
2024-01-26 05:37:30 +0900

    [#21] Feat: 다른 유저 페이지 UI 구현 (#22)

---
+++ src/components/MyActivityPage/ReceivedIngredientsList.tsx
@@ -0,0 +1,19 @@
+import ReceivedIngredientsListItem from "./ReceivedIngredientsListItem";
+
+const ReceivedIngredientsList = () => {
+  return (
+    <ul>
+      <ReceivedIngredientsListItem />
+      <ReceivedIngredientsListItem />
+      <ReceivedIngredientsListItem />
+      <ReceivedIngredientsListItem />
+      <ReceivedIngredientsListItem />
+      <ReceivedIngredientsListItem />
+      <ReceivedIngredientsListItem />
+      <ReceivedIngredientsListItem />
+      <ReceivedIngredientsListItem />
+    </ul>
+  );
+};
+
+export default ReceivedIngredientsList;
--- src/pages/RandomUserPage.tsx
+++ src/pages/RandomUserPage.tsx
@@ -1,5 +1,70 @@
+import { Fragment } from "react";
+
+import { css } from "@styled-system/css";
+
+import Header from "@/components/common/Header";
+import IconButton from "@/components/common/IconButton";
+import TteokgukList from "@/components/common/TteokgukList";
+import GuideIcon from "@/assets/svg/profile.svg";
+import DumplingIcon from "@/assets/svg/dumpling.svg";
+import VisitIcon from "@/assets/svg/visit.svg";
+import UserProfileSection from "@/components/common/UserProfileSection";
+
 const RandomUserPage = () => {
-  return <div>랜덤 방문한 떡국 페이지입니다.</div>;
+  return (
+    <Fragment>
+      <Header hasPreviousPage actionIcon={<GuideIcon />}>
+        프로필
+      </Header>
+      <div className={styles.container}>
+        <UserProfileSection
+          nickname="사용자 닉네임"
+          UniqueIngredientIcon={<DumplingIcon />}
+          color="primary"
+        />
+        <div className={styles.buttonContainer}>
+          <IconButton color="primary.45" applyColorTo="outline">
+            <IconButton.Icon>
+              <VisitIcon />
+            </IconButton.Icon>
+            랜덤 방문
+          </IconButton>
+        </div>
+        <div>
+          <div className={styles.wishTteokgukTitle}>
+            <div>사용자님의 소원 떡국 리스트</div>
+          </div>
+          <TteokgukList className={styles.tteokgukList} />
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default RandomUserPage;
+
+const styles = {
+  container: css({
+    height: "calc(100% - 5.6rem)",
+    padding: "0 2.4rem 2.4rem",
+    marginBottom: "0.8rem",
+  }),
+  buttonContainer: css({
+    display: "flex",
+    gap: "0.8rem",
+  }),
+  wishTteokgukTitle: css({
+    display: "flex",
+    justifyContent: "space-between",
+    fontWeight: 700,
+
+    marginTop: "3.2rem",
+  }),
+  tteokgukList: css({
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, 9.6rem)",
+    justifyContent: "space-between",
+    rowGap: "1.2rem",
+    marginTop: "1.2rem",
+  }),
+};
