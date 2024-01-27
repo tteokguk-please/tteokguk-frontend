10b69479a4cd4a3d42cc37dbaab0539985a8fc64
Minsoo Park <sparkm0128@gmail.com>
2024-01-28 04:57:18 +0900

    [#34] Feat: 회원가입 API 연동 (#40)

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,1 +1,5 @@
 export type Filter<TOrigin, TAllowed> = TOrigin extends TAllowed ? TOrigin : never;
+export type KeyOf<T> = keyof T;
+export type ValueOf<T> = T extends Array<unknown> | Readonly<Array<unknown>>
+  ? T[number]
+  : T[keyof T];
