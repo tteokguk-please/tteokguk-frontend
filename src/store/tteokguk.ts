d15d1c94916a02e28cae278970bed034ebeed39c
Kim-Jaemin420 <jany240@naver.com>
2024-01-28 02:44:53 +0900

    Feat: 성공시 떡국 상세 피이지로 이동

--- src/store/tteokguk.ts
+++ src/store/tteokguk.ts
@@ -2,8 +2,19 @@ import { atomWithMutation } from "jotai-tanstack-query";
 
 import { postTteokguk } from "@/apis/tteokguk";
 
-import { PostTteokgukRequest } from "@/types/tteokguk";
+import { PostTteokgukRequest, PostTteokgukResponse } from "@/types/tteokguk";
 
-export const $postTteokguk = atomWithMutation(() => ({
-  mutationFn: (tteokguk: PostTteokgukRequest) => postTteokguk(tteokguk),
-}));
+import useRouter from "@/routes/useRouter";
+
+export const $postTteokguk = atomWithMutation(() => {
+  const navigate = useRouter();
+
+  return {
+    mutationFn: (tteokguk: PostTteokgukRequest) => postTteokguk(tteokguk),
+    onSuccess: (tteokgukResponse: PostTteokgukResponse) => {
+      const { tteokgukId } = tteokgukResponse;
+
+      navigate.push(`/tteokguks/${tteokgukId}`);
+    },
+  };
+});
