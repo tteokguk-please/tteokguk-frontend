2a13954e11d09c418654731b6bfd9f309d76e91c
Kim-Jaemin420 <jany240@naver.com>
2024-01-30 01:02:13 +0900

    Modify: 배달 용용이 이미지 추가 및 레이아웃 재배치

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,28 +2,38 @@ import { Fragment } from "react";
 
 import { css } from "@styled-system/css";
 
-import Header from "@/components/common/Header";
-import headerLogo from "@/assets/images/header-logo.png";
-import Button from "@/components/common/Button";
 import { Link } from "@/routes/Link";
+import Header from "@/components/common/Header";
+import Button from "@/components/common/Button";
+import deliveryDragon from "@/assets/images/delivery-dragon.png";
+import KakaoIcon from "@/assets/svg/kakao.svg";
+
 const LoginPage = () => {
   return (
     <Fragment>
       <Header hasPreviousPage>
-        <Link to="/">
-          <img src={headerLogo} alt="용용이 로고" />
-        </Link>
+        <Link to="/">로그인</Link>
       </Header>
-      <div className={styles.buttonContainer}>
-        <Link to="/login/email">
-          <Button color="primary.45" applyColorTo="outline" className={styles.emailLoginButton}>
-            이메일로 로그인
+      <div className={styles.container}>
+        <div>
+          <img src={deliveryDragon} alt="배달하는 용용이" />
+        </div>
+        <div className={styles.buttonContainer}>
+          <Link to="/login/email">
+            <Button color="primary.45" applyColorTo="outline" className={styles.emailLoginButton}>
+              이메일로 로그인
+            </Button>
+          </Link>
+          <Button className={styles.kakaoLoginButton}>
+            <div>
+              <KakaoIcon />
+            </div>
+            <span className={styles.kakao}>카카오로 로그인</span>
           </Button>
-        </Link>
-        <Button className={styles.kakaoLoginButton}>카카오로 로그인</Button>
-        <Link to="/signup" className={styles.signupLink}>
-          회원가입 하러가기&gt;
-        </Link>
+          <Link to="/signup" className={styles.signupLink}>
+            회원가입 하러가기&gt;
+          </Link>
+        </div>
       </div>
     </Fragment>
   );
@@ -32,20 +42,31 @@ const LoginPage = () => {
 export default LoginPage;
 
 const styles = {
-  buttonContainer: css({
+  container: css({
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center",
     width: "100%",
     height: "calc(100vh - 4.8rem)",
     padding: "0 4rem",
+  }),
+  buttonContainer: css({
+    width: "100%",
   }),
   emailLoginButton: css({
     marginBottom: "1rem",
   }),
   kakaoLoginButton: css({
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: "8rem",
+    paddingX: "2.5rem",
     backgroundColor: "#FEE500",
+  }),
+  kakao: css({
+    flexGrow: 1,
   }),
   signupLink: css({
     display: "block",
