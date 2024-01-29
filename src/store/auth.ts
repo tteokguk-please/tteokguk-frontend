137f7fac542a06df18c2b8597625ff908df8258f
minsour <sparkm0128@gmail.com>
2024-01-30 00:24:56 +0900

    Modify: 쓸모없는 mutationKey 제거

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -22,17 +22,14 @@ import {
 } from "@/types/auth";
 
 export const $checkEmail = atomWithMutation(() => ({
-  mutationKey: ["checkEmail"],
   mutationFn: (email: string): Promise<CheckEmailNicknameResponse> => checkEmail(email),
 }));
 
 export const $checkNickname = atomWithMutation(() => ({
-  mutationKey: ["checkNickname"],
   mutationFn: (nickname: string): Promise<CheckEmailNicknameResponse> => checkNickname(nickname),
 }));
 
 export const $signup = atomWithMutation(() => ({
-  mutationKey: ["signup"],
   mutationFn: (body: SignupRequest): Promise<SignupResponse> => postSignup(body),
 }));
 
