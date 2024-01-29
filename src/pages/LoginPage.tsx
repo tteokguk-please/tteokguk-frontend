a8bc23d8f83c80f7b3b29deae23e36f90e66e8ce
minsour <sparkm0128@gmail.com>
2024-01-29 19:22:49 +0900

    Feat: LoginPage에 카카오 로그인 적용

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 
 import { css } from "@styled-system/css";
 
@@ -6,7 +6,66 @@ import Header from "@/components/common/Header";
 import headerLogo from "@/assets/images/header-logo.png";
 import Button from "@/components/common/Button";
 import { Link } from "@/routes/Link";
+import { useSearchParams } from "react-router-dom";
+import { useAtomValue } from "jotai";
+import { $postKakaoLogin, $postKakaoToken } from "@/store/auth";
+import { KakaoLoginResponse, KakaoTokenReponse } from "@/types/auth";
+import useRouter from "@/routes/useRouter";
+import { RoutePath } from "@/routes/Routes";
+
 const LoginPage = () => {
+  const [searchParams] = useSearchParams();
+  const router = useRouter();
+
+  const { mutate: postKakaoToken } = useAtomValue($postKakaoToken);
+  const { mutate: postKakaoLogin } = useAtomValue($postKakaoLogin);
+
+  const handleClickKakaoLogin = () => {
+    const kakaoToken = localStorage.getItem("kakaoToken");
+
+    if (kakaoToken) {
+      postKakaoLogin(
+        { accessToken: kakaoToken },
+        {
+          onSuccess: handleSuccessPostKakaoLogin,
+        },
+      );
+    } else {
+      window.location.href = import.meta.env.VITE_KAKAO_LOGIN_URI;
+    }
+  };
+
+  const handleSuccessPostKakaoToken = ({ access_token }: KakaoTokenReponse) => {
+    localStorage.setItem("kakaoToken", access_token);
+    postKakaoLogin(
+      { accessToken: access_token },
+      {
+        onSuccess: handleSuccessPostKakaoLogin,
+      },
+    );
+  };
+
+  const handleSuccessPostKakaoLogin = ({
+    accessToken,
+    refreshToken,
+    isInitialized,
+  }: KakaoLoginResponse) => {
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("refreshToken", refreshToken);
+
+    const nextPath: RoutePath = isInitialized ? "/tteokguks" : "/nickname/create";
+    router.push(nextPath);
+  };
+
+  useEffect(() => {
+    const kakaoUserCode = searchParams.get("code");
+    if (kakaoUserCode) {
+      postKakaoToken(kakaoUserCode, {
+        onSuccess: handleSuccessPostKakaoToken,
+      });
+    }
+  }, [searchParams, postKakaoToken]);
+
   return (
     <Fragment>
       <Header hasPreviousPage>
@@ -20,7 +79,9 @@ const LoginPage = () => {
             이메일로 로그인
           </Button>
         </Link>
-        <Button className={styles.kakaoLoginButton}>카카오로 로그인</Button>
+        <Button className={styles.kakaoLoginButton} onClick={handleClickKakaoLogin}>
+          카카오로 로그인
+        </Button>
         <Link to="/signup" className={styles.signupLink}>
           회원가입 하러가기&gt;
         </Link>
