395a7bc2cddf29e5ea28b8f83f05482ac09bdca0
Kim-Jaemin420 <jany240@naver.com>
2024-01-30 02:32:09 +0900

    Feat: 모달에 변경된 이미지 적용

--- src/components/shared/GuideModal.tsx
+++ src/components/shared/GuideModal.tsx
@@ -4,7 +4,9 @@ import { css } from "@styled-system/css";
 
 import Modal from "@/components/common/modal/Modal";
 import Button from "@/components/common/Button";
-import DumplingIcon from "@/assets/svg/dumpling.svg";
+import TteokgukIcon from "@/assets/svg/tteokguk.svg";
+import GiftIcon from "@/assets/svg/gift.svg";
+import LuckyBagIcon from "@/assets/svg/lucky-bag.svg";
 
 interface Props {
   isOpen: boolean;
@@ -18,16 +20,19 @@ const GuideModal = ({ isOpen, onClose }: Props) => {
     {
       title: "소원 떡국을 만들어보세요!",
       content: `새해 소원을 작성하고,\n 떡국에 들어갈 재료 5개를 선택해주세요.`,
+      icon: <TteokgukIcon aria-label="떡국" />,
       buttonContent: "다음",
     },
     {
       title: "재료를 선물해보세요!",
       content: "다른 사람들에게 떡국 재료를 나눠주면 복주머니를 얻을 수 있어요.",
+      icon: <GiftIcon aria-label="선물" />,
       buttonContent: "다음",
     },
     {
       title: "복주머니를 열어보세요!",
       content: "복주머니를 열면 새로운 떡국 재료를\n 랜덤으로 얻을 수 있어요.",
+      icon: <LuckyBagIcon aria-label="복주머니" />,
       buttonContent: "확인",
     },
   ];
@@ -49,9 +54,7 @@ const GuideModal = ({ isOpen, onClose }: Props) => {
         <Modal.Header className={styles.title}>{modalContents[step].title}</Modal.Header>
         <Modal.Body className={styles.bodyContainer}>
           <div className={styles.content}>{modalContents[step].content}</div>
-          <div className={styles.ingredientImage} aria-label="만두">
-            <DumplingIcon />
-          </div>
+          <div className={styles.imageContainer}>{modalContents[step].icon}</div>
           <Button onClick={handleClickNextButton} color="primary.100" applyColorTo="background">
             {modalContents[step].buttonContent}
           </Button>
@@ -85,7 +88,7 @@ const styles = {
     paddingX: "3rem",
     whiteSpace: "pre-line",
   }),
-  ingredientImage: css({
+  imageContainer: css({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
