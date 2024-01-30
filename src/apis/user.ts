dfb1bcc683f602c67e06eeceb2567f5aedb3df5f
Kim-Jaemin420 <jany240@naver.com>
2024-01-30 12:46:02 +0900

    Feat: 내 정보 가져오는 api 함수의 반환 타입 정의

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,3 +1,5 @@
+import { MyDetailsResponse } from "@/types/user.dto";
+
 import http from "./core";
 
-export const getMyDetails = () => http.get("api/v1/user/myPage");
+export const getMyDetails = () => http.get<MyDetailsResponse>("api/v1/user/myPage");
