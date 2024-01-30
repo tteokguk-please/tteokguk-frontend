a3835fd265e6188ce768e2c255aa91ad4730100a
Kim-Jaemin420 <jany240@naver.com>
2024-01-31 01:24:44 +0900

    Modify: Tteokguk 타입에 completion 추가

--- src/types/tteokguk.ts
+++ src/types/tteokguk.ts
@@ -3,6 +3,7 @@ import { IngredientKey } from "./ingredient";
 export interface Tteokguk {
   tteokgukId: number;
   wish: string;
+  completion: boolean;
   access: boolean;
   tteokgukIngredients: IngredientKey[];
 }
