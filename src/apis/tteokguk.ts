aa5f88d8b48ab7bf21535f29631ded8d0a8c108a
Kim-Jaemin420 <jany240@naver.com>
2024-02-01 02:03:18 +0900

    Feat: 메인페이지에서 탭별로 떡국 리스트 가져오는 api 함수 구현

--- src/apis/tteokguk.ts
+++ src/apis/tteokguk.ts
@@ -1,6 +1,18 @@
-import { PostTteokgukRequest, PostTteokgukResponse } from "@/types/tteokguk.dto";
+import {
+  GetNewTteokguksReponse,
+  PostTteokgukRequest,
+  PostTteokgukResponse,
+} from "@/types/tteokguk.dto";
 
 import http from "./core";
+
+const SIZE = 20;
+
+export const getNewTteokguks = (page: number) =>
+  http.get<GetNewTteokguksReponse>(`api/v1/tteokguk/new?size=${SIZE}&page=${page}`);
+
+export const getCompletedTteokguks = (page: number) =>
+  http.get<GetNewTteokguksReponse>(`api/v1/tteokguk/completion?size=${SIZE}&page=${page}`);
 
 export const postTteokguk = (tteokguk: PostTteokgukRequest) =>
   http.post<PostTteokgukResponse>("api/v1/tteokguk", tteokguk);
