41843773b72bcb7720270625be384d4c88a4f5dd
Kim-Jaemin420 <jany240@naver.com>
2024-02-01 02:05:13 +0900

    Feat: 토큰 만료시, 내 정보 요청 안하도록 구현

--- src/store/user.ts
+++ src/store/user.ts
@@ -3,11 +3,18 @@ import { atomWithSuspenseQuery } from "jotai-tanstack-query";
 import { getMyDetails, getUserDetails } from "@/apis/user";
 
 import { atomFamilyWithSuspenseQuery } from "@/utils/jotai";
+import { getLocalStorage } from "@/utils/localStorage";
+import { isExpiredAccessToken } from "@/utils/token";
 
-export const $getMyDetails = atomWithSuspenseQuery(() => ({
-  queryKey: ["myDetails"],
-  queryFn: getMyDetails,
-}));
+export const $getMyDetails = atomWithSuspenseQuery(() => {
+  const isTokenExpired = isExpiredAccessToken(getLocalStorage("accessToken"));
+
+  return {
+    queryKey: ["myDetails"],
+    queryFn: () => getMyDetails(),
+    enabled: isTokenExpired,
+  };
+});
 
 export const $getUserDetail = atomFamilyWithSuspenseQuery("users", (id: number) => {
   return getUserDetails(id);
