84b3c3f7d322b700f2a4e6bd528a8adaa5580bf3
Kim-Jaemin420 <jany240@naver.com>
2024-02-02 01:25:37 +0900

    Rename: SIZE -> TTEOKGUKS_SIZE

--- src/apis/tteokguk.ts
+++ src/apis/tteokguk.ts
@@ -6,13 +6,15 @@ import {
 
 import http from "./core";
 
-const SIZE = 20;
+const TTEOKGUKS_SIZE = 20;
 
 export const getNewTteokguks = (page: number) =>
-  http.get<GetNewTteokguksReponse>(`api/v1/tteokguk/new?size=${SIZE}&page=${page}`);
+  http.get<GetNewTteokguksReponse>(`api/v1/tteokguk/new?size=${TTEOKGUKS_SIZE}&page=${page}`);
 
 export const getCompletedTteokguks = (page: number) =>
-  http.get<GetNewTteokguksReponse>(`api/v1/tteokguk/completion?size=${SIZE}&page=${page}`);
+  http.get<GetNewTteokguksReponse>(
+    `api/v1/tteokguk/completion?size=${TTEOKGUKS_SIZE}&page=${page}`,
+  );
 
 export const postTteokguk = (tteokguk: PostTteokgukRequest) =>
   http.post<PostTteokgukResponse>("api/v1/tteokguk", tteokguk);
