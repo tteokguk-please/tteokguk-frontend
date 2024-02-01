9c0aefd08d94794c19f67004e1219b2e792b9e01
Kim-Jaemin420 <jany240@naver.com>
2024-02-02 01:17:43 +0900

    Feat: 내 정보 가져오는 api 401 에러일때 null 반환

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,4 +1,5 @@
 import { atomWithSuspenseQuery } from "jotai-tanstack-query";
+import { HTTPError } from "ky";
 
 import { getMyDetails, getUserDetails } from "@/apis/user";
 
@@ -6,7 +7,15 @@ import { atomFamilyWithSuspenseQuery } from "@/utils/jotai";
 
 export const $getMyDetails = atomWithSuspenseQuery(() => ({
   queryKey: ["myDetails"],
-  queryFn: getMyDetails,
+  queryFn: async () => {
+    try {
+      return await getMyDetails();
+    } catch (error) {
+      if (error instanceof HTTPError) {
+        if (error.response.status === 401) return null;
+      }
+    }
+  },
 }));
 
 export const $getUserDetail = atomFamilyWithSuspenseQuery("users", (id: number) => {
