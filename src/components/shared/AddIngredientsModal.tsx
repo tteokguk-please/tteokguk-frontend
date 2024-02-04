1a477e72ad31695a42414879cb9eea9bce561891
Kim-Jaemin420 <jany240@naver.com>
2024-02-04 15:00:38 +0900

    Modify: 재료 컴포넌트 분리

--- src/components/shared/AddIngredientsModal.tsx
+++ src/components/shared/AddIngredientsModal.tsx
@@ -1,11 +1,15 @@
+import { useAtom } from "jotai";
+
 import { css } from "@styled-system/css";
 
 import { LoggedInUserDetailsResponse } from "@/types/user.dto";
 import { IngredientKey } from "@/types/ingredient";
 
+import Ingredient from "../TteokgukCookingPage/Ingredient";
+
 import Modal from "@/components/common/modal/Modal";
 import Button from "@/components/common/Button";
-import { INGREDIENT_ICON_BY_KEY, INGREDIENT_KEYS } from "@/constants/ingredient";
+import { $updateSelectedIngredients } from "@/store/ingredient";
 
 interface Props {
   isOpen: boolean;
@@ -15,8 +19,17 @@ interface Props {
 }
 
 const AddIngredientsModal = ({ isOpen, onClose, memberId, loggedInUserDetails }: Props) => {
+  const [selectedIngredients, updateSelectedIngredients] = useAtom($updateSelectedIngredients);
+
+  const { itemResponses: ingredientsStocks } = loggedInUserDetails;
   const title = loggedInUserDetails.id === memberId ? "떡국 재료 추가하기" : "떡국 재료 보내기";
   const buttonText = title === "떡국 재료 추가하기" ? "추가하기" : "다음";
+
+  const handleClickIngredient = (ingredientKey: IngredientKey) => () => {
+    console.log(ingredientKey);
+
+    updateSelectedIngredients(ingredientKey);
+  };
 
   return (
     isOpen && (
@@ -27,8 +40,14 @@ const AddIngredientsModal = ({ isOpen, onClose, memberId, loggedInUserDetails }:
         <Modal.Body className={styles.contentContainer}>
           <div className={styles.bodyTitle}>내가 가지고 있는 재료</div>
           <ol className={styles.content}>
-            {INGREDIENT_KEYS.map((ingredientKey) => (
-              <Ingredient key={ingredientKey} ingredientKey={ingredientKey} />
+            {ingredientsStocks.map(({ ingredient, stockQuantity }) => (
+              <Ingredient
+                key={ingredient}
+                ingredientKey={ingredient}
+                handleClickIngredient={handleClickIngredient(ingredient)}
+                stockQuantity={stockQuantity}
+                isSelected={selectedIngredients.includes(ingredient)}
+              />
             ))}
           </ol>
           <Button onClick={onClose} color="primary.100" applyColorTo="background">
@@ -41,17 +60,6 @@ const AddIngredientsModal = ({ isOpen, onClose, memberId, loggedInUserDetails }:
 };
 
 export default AddIngredientsModal;
-
-const Ingredient = ({ ingredientKey }: { ingredientKey: IngredientKey }) => {
-  const IngredientIcon = INGREDIENT_ICON_BY_KEY[40][ingredientKey];
-
-  return (
-    <li className={styles.ingredient} aria-label="만두">
-      <div className={styles.ingredientNumber}>1</div>
-      <IngredientIcon />
-    </li>
-  );
-};
 
 const styles = {
   container: css({
@@ -86,29 +94,5 @@ const styles = {
     borderBottomRadius: "0.8rem",
     marginBottom: "2rem",
     padding: "1.2rem",
-  }),
-  ingredient: css({
-    position: "relative",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "5.6rem",
-    height: "5.6rem",
-    borderRadius: "50%",
-    backgroundColor: "secondary.100",
-    cursor: "pointer",
-  }),
-  ingredientNumber: css({
-    position: "absolute",
-    top: 0,
-    left: "4rem",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "2rem",
-    height: "2rem",
-    fontSize: "1rem",
-    backgroundColor: "primary.100",
-    borderRadius: "50%",
   }),
 };
