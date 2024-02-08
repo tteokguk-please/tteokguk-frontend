493eacef3cc61f6023022b1207fe13cdae0a70a4
Kim-Jaemin420 <jany240@naver.com>
2024-02-09 00:38:23 +0900

    Style: background image -> img

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,6 +6,7 @@ import homeLogo from "@/assets/images/home-logo.png";
 import tryDragon from "@/assets/images/try-dragon.png";
 import leftMountain from "@/assets/images/left-mountain.png";
 import rightMountain from "@/assets/images/right-mountain.png";
+import homePattern from "@/assets/images/home-pattern.png";
 
 const HomePage = () => {
   return (
@@ -15,6 +16,9 @@ const HomePage = () => {
           <img src={homeLogo} alt="로고" />
         </Link>
       </h1>
+      <div>
+        <img src={homePattern} alt="배경" className={styles.backgroundImage} />
+      </div>
       <div className={styles.dragon}>
         <img src={tryDragon} alt="떡국 권하는 용용이" />
       </div>
@@ -55,6 +59,15 @@ const styles = {
     backgroundRepeat: "repeat-Y",
     backgroundImage: `url(src/assets/images/home-pattern.png)`,
     overflow: "hidden",
+  }),
+  backgroundImage: css({
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    objectFit: "cover", // 또는 'contain'에 따라
+    zIndex: -1, // 다른 내용 위에 배치
   }),
   logo: css({
     width: "21rem",
