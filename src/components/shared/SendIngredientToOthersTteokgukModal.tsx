ca741f0fa93ad88f8f2367633cefabd152060a66
Minsoo Park <sparkm0128@gmail.com>
2024-02-09 18:22:42 +0900

    [NO-ISSUE] Fix: $selectedIngredient 초기화 잘 되도록 수정 (#145)

--- src/components/shared/SendIngredientToOthersTteokgukModal.tsx
+++ src/components/shared/SendIngredientToOthersTteokgukModal.tsx
@@ -13,6 +13,7 @@ import Button from "../common/Button";
 import CreateCheerMessageModal from "./CreateCheerMessageModal";
 
 import { $updateSelectedIngredient } from "@/store/ingredient";
+import { useEffect } from "react";
 
 interface Props {
   isOpen: boolean;
@@ -45,6 +46,7 @@ const SendIngredientsToOthersTteokgukModal = ({
       <CreateCheerMessageModal
         isOpen={isOpen}
         onClose={() => {
+          updateSelectedIngredient(null);
           handleCloseCheerMessageModal();
           onClose();
         }}
@@ -57,6 +59,12 @@ const SendIngredientsToOthersTteokgukModal = ({
     updateSelectedIngredient(null);
     onClose();
   };
+
+  useEffect(() => {
+    return () => {
+      updateSelectedIngredient(null);
+    };
+  }, [updateSelectedIngredient]);
 
   return (
     isOpen && (
@@ -81,7 +89,12 @@ const SendIngredientsToOthersTteokgukModal = ({
                 ),
             )}
           </ol>
-          <Button onClick={handleClickNextButton} color="primary.100" applyColorTo="background">
+          <Button
+            disabled={!selectedIngredient}
+            onClick={handleClickNextButton}
+            color="primary.100"
+            applyColorTo="background"
+          >
             다음
           </Button>
         </Modal.Body>
