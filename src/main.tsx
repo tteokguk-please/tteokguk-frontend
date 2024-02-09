2f792947368806ab07ef6fd36174fd336b790353
Kim-Jaemin420 <jany240@naver.com>
2024-02-09 20:25:05 +0900

    Feat: sentry 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,18 @@
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
 
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: [/^https:\/\/www\.tteokguk-please\.com/],
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Sentry.ErrorBoundary>
+    <App />
+  </Sentry.ErrorBoundary>,
+);
