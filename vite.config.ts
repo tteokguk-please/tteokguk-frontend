d89f165437c516a2719be8777b3614babf8bfd12
Kim-Jaemin420 <jany240@naver.com>
2024-02-09 18:01:18 +0900

    Chore: gtag 설치 및 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import ViteRadar from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,12 @@ export default defineConfig({
     react(),
     svgr({
       include: "**/*.svg",
+    }),
+    ViteRadar({
+      enableDev: true,
+      analytics: {
+        id: process.env.VITE_GA_TRACKING_ID,
+      },
     }),
   ],
   resolve: {
