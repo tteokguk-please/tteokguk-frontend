3534bbb9d72f56a6c572174ad7d50525292b6a70
Kim-Jaemin420 <jany240@naver.com>
2024-02-11 04:11:00 +0900

    Feat: 유저 프로필 섹션에 공유하기 추가

--- src/components/common/UserProfileSection.tsx
+++ src/components/common/UserProfileSection.tsx
@@ -1,28 +1,47 @@
 import { css } from "@styled-system/css";
 
+import { copyLink } from "@/utils/linkShare";
+
 import { IngredientKey } from "@/types/ingredient";
 
 import { INGREDIENT_ICON_BY_KEY } from "@/constants/ingredient";
 
 interface Props {
+  id: number;
   nickname: string;
   uniqueIngredientKey: IngredientKey;
   color: "primary" | "secondary";
   className?: string;
+  isMyPage?: boolean;
 }
 
-const UserProfileSection = ({ nickname, uniqueIngredientKey, color }: Props) => {
+const UserProfileSection = ({
+  id,
+  nickname,
+  uniqueIngredientKey,
+  color,
+  isMyPage = false,
+}: Props) => {
   const IngredientIcon = INGREDIENT_ICON_BY_KEY[40][uniqueIngredientKey];
+
+  const handleClickShareButton = () => {
+    copyLink({
+      path: `/users/${id}`,
+      eventCategory: isMyPage ? "마이페이지 링크 공유 클릭" : "다른 유저 페이지 링크 공유 클릭",
+    });
+  };
 
   return (
     <article className={styles.userInfo(color)}>
-      <div>{nickname}님</div>
       <div className={styles.uniqueIngredient}>
-        고유재료
         <div className={styles.ingredientIcon(color)}>
           <IngredientIcon />
         </div>
+        <div>{nickname}님</div>
       </div>
+      <button onClick={handleClickShareButton} className={styles.shareButton(color)}>
+        공유하기
+      </button>
     </article>
   );
 };
@@ -55,7 +74,7 @@ const styles = {
       height: "5.2rem",
       borderRadius: "50%",
       backgroundColor: color === "primary" ? "primary.20" : "secondary.50",
-      marginLeft: "1rem",
+      marginRight: "0.6rem",
     }),
   primary: css({
     backgroundColor: "primary.100",
@@ -63,4 +82,13 @@ const styles = {
   secondary: css({
     backgroundColor: "secondary.100",
   }),
+  shareButton: (color: "primary" | "secondary") =>
+    css({
+      fontSize: "1.4rem",
+      fontWeight: 600,
+      width: "6.5rem",
+      backgroundColor: color === "primary" ? "primary.20" : "secondary.50",
+      borderRadius: "0.4rem",
+      paddingY: "0.4rem",
+    }),
 };
